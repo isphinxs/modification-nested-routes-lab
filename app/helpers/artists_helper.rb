e9f7b43d0efa2f3372851c1408359bcdda7ec23f
Samantha Ostrowski <isphinxs@hotmail.com>
2021-04-14 20:35:11 -0400

    commit tree 7bb14773480934080b9e817b4c0a018a3e6a0864

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,9 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_select(song, artists)
+    # byebug
+    song.artist.nil? ? select_tag("song[artist_id]", options_from_collection_for_select(artists, "id", "name", include_blank: false, include_hidden: false)) : hidden_field_tag("song[artist_id]", song.artist.id)
+  end
 end
